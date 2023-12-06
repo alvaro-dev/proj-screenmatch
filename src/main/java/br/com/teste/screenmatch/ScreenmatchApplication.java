24996028002bd02fbd9c2361142d05ff2b3af4f9
Alvaro Rocha <alvarorocha@protonmail.com>
2023-12-06 15:42:13 -0300

    Implementado Colecao de Dados

--- src/main/java/br/com/teste/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/teste/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,17 @@
 package br.com.teste.screenmatch;
 
+import br.com.teste.screenmatch.model.DadosEpisodio;
 import br.com.teste.screenmatch.model.DadosSerie;
+import br.com.teste.screenmatch.model.DadosTemporada;
+import br.com.teste.screenmatch.principal.Principal;
 import br.com.teste.screenmatch.service.ConsumoApi;
 import br.com.teste.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,12 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Session=1&apikey=55b7545b");
-		System.out.println(json);
-		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibemenu();
 	}
 }
